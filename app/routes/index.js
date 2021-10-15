b2736e70bcd27fb40ccb52d3b7aa3da3159737ae
Poonnaluk <610107030035@dpu.ac.th>
2021-10-15 17:11:09 +0700

    Change Get to Post on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ const { getUser, createUser } = require("../controllers/users");
 const { createRoom } = require("../controllers/rooms");
 
 module.exports = function (app) {
-  app.get("/user/:email", getUser);
+  app.post("/user/:email", getUser);
   app.post("/user", createUser);
 
   app.post("/room", createRoom);
