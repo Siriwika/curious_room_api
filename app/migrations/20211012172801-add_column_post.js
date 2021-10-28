e5d6ea66b8860183684bf906b8c048d0dce387d7
Siriwika <siriwika574@gmail.com>
2021-10-28 17:38:52 +0700

    change column commentId in Table Posts to allowed null

--- app/migrations/20211012172801-add_column_post.js
+++ app/migrations/20211012172801-add_column_post.js
@@ -3,27 +3,21 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     // logic for transforming into the new state
-    return queryInterface.addColumn(
+    return queryInterface.changeColumn(
       'Posts',
      'commentId', {
         type: Sequelize.INTEGER,
-        allowNull: false,
-        onDelete: "CASCADE",
-        references: {
-          model: "Comments",
-          key: "id",
-          as: "best_comment",
-        }
+        allowNull: true,
       },
     );
 
   },
 
-  down: function(queryInterface, Sequelize) {
-    // logic for reverting the changes
-    return queryInterface.removeColumn(
-      'Posts',
-      'commentId'
-    );
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn('Posts', 'commentId', {
+      type: Sequelize.INTEGER,
+      allowNull: false
+      
+  },)
   }
 }
