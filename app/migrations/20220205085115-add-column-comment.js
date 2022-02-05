729c1d098dfec37b5f9bd739f04df228fe07e05d
Siriwika <siriwika574@gmail.com>
2022-02-05 16:46:51 +0700

    create migrate addcolumn confirmStatus

---
+++ app/migrations/20220205085115-add-column-comment.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("Comments", "confirmStatus", {
+      allowNull: false,
+      type: Sequelize.BOOLEAN,
+      defaultValue: 0,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn("Comments", "confirmStatus", {
+      allowNull: false,
+      type: Sequelize.BOOLEAN,
+      defaultValue: 0,
+    });
+  },
+};
